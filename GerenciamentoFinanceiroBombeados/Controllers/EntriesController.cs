12b164f0b4c01a44e92f73949e636879ce5381b7
jonathandr9 <jonathan>
2020-07-26 02:45:22 -0300

    Create Aplication Asp .Net MVC

---
+++ GerenciamentoFinanceiroBombeados/Controllers/EntriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciamentoFinanceiroBombeados.Controllers
+{
+    public class EntriesController : Controller
+    {
+        // GET: Entries
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GerenciamentoFinanceiroBombeados/Controllers/OutputsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciamentoFinanceiroBombeados.Controllers
+{
+    public class OutputsController : Controller
+    {
+        // GET: Outputs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
